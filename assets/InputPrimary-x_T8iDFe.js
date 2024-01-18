a4c35e58ebfcaa6e9d14023d9bb625d5bcab8509
Robertw8 <126002887+Robertw8@users.noreply.github.com>
2024-01-18 23:35:03 +0000

    Deploying to gh-pages from @ Robertw8/fitness-app-frontend@5c59a38340e54c3d5e91016783da8bd7277538bb 🚀

--- assets/InputPrimary-x_T8iDFe.js
+++
@@ -1,1 +0,0 @@
-import{j as n,S as o}from"./index-7T5PGSjv.js";const a=({placeholder:t,bordercolor:r,...s})=>n.jsx(o,{placeholder:t,bordercolor:r,...s});export{a as I};
--- assets/SignUp.styled-msPNSg6w.js
+++ assets/SignUp.styled-msPNSg6w.js
@@ -1,4 +1,4 @@
-import{n as o,b as n}from"./index-7T5PGSjv.js";const i=o.form`
+import{n as o,b as n}from"./index-FVnREtQj.js";const i=o.form`
 width: 500px;
 display: flex;
 flex-direction: column;
