9ef11d98ad365435904ceb92eaef470992320e96
Robertw8 <Robertw8@users.noreply.github.com>
2024-01-18 18:12:00 +0000

    deploy: f4425f37b0a23274c352a4ee07d06eb572d588c6

--- assets/SignUp.styled-MGfde3lZ.js
+++ assets/SignUp.styled-MGfde3lZ.js
@@ -1,4 +1,4 @@
-import{n as o,b as n}from"./index-Mj_xod9n.js";const i=o.form`
+import{n as o,b as n}from"./index-yPwuo5FO.js";const i=o.form`
 width: 500px;
 display: flex;
 flex-direction: column;
