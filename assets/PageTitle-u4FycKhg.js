8870cfc6100f9f477a52a05092e629f8048f31c1
Robertw8 <126002887+Robertw8@users.noreply.github.com>
2024-01-18 20:43:40 +0000

    Deploying to gh-pages from @ Robertw8/fitness-app-frontend@92fb05f8694c0a7be8cda080d059d10b2e31f7bb 🚀

---
+++ assets/PageTitle-u4FycKhg.js
@@ -0,0 +1,1 @@
+import{j as e,bz as r}from"./index-VAh8bqNJ.js";const i=({text:t})=>e.jsx(r,{children:t});export{i as P};
---
+++ assets/imgForWelcomePage-t-lFdlDu.js
@@ -0,0 +1,1 @@
+const e="/fitness-app-frontend/assets/imgForWelcomePage-sUIJYmJw.jpg";export{e as i};
